c1439f6736fdf3a2dcff5dfceea761fba22307f4
Justin Lam <justinmklam@gmail.com>
2019-03-24 10:29:58 -0700

    [major] Refactor using task schedulers

--- chicken_door_controller/chicken_door_controller.ino
+++ chicken_door_controller/chicken_door_controller.ino
@@ -2,6 +2,9 @@
 #include "ssd1306.h"
 #include "rtc_ds3231.h"
 #include "motor_tb6612.h"
+// #include <Arduino_FreeRTOS.h>
+#include "Task.h"
+#include "TaskScheduler.h"
 
 Rtc rtc;      //we are using the DS3231 RTC
 Ssd1306 display;
@@ -19,48 +22,19 @@ const uint8_t buttonPinRight = 12;
 // Pullup resistor used
 const uint8_t defaultButtonState = HIGH;
 
-
 const uint16_t intervalDisplayRefresh = 1000;
 const uint16_t intervalInactive = 10e3;
-
-void powerDown() {
-  display.sleep();
-
-  attachInterrupt(digitalPinToInterrupt(wakePin), wakeUpInterruptHandler, LOW);
-  attachInterrupt(digitalPinToInterrupt(buttonPinMiddle), wakeUpInterruptHandler, CHANGE);
-
-  LowPower.powerDown(SLEEP_FOREVER, ADC_OFF, BOD_OFF);
-}
-
-void wakeUp() {
-  display.wake();
-
-  detachInterrupt(digitalPinToInterrupt(wakePin));                                    //execution resumes from here after wake-up
-  detachInterrupt(digitalPinToInterrupt(buttonPinMiddle));
-}
-
-//-------------------------------------------------
-
-void wakeUpInterruptHandler()        // here the interrupt is handled after wakeup
-{
-}
 
 //------------------------------------------------------------
 
 void setup() {
-  //Set pin D2 as INPUT for accepting the interrupt signal from DS3231
-  pinMode(wakePin, INPUT);
-  pinMode(buttonPinLeft, INPUT);
-  pinMode(buttonPinMiddle, INPUT);
-  pinMode(buttonPinRight, INPUT);
 
   //switch-on the on-board led for 1 second for indicating that the sketch is ok and running
   pinMode(ledPin, OUTPUT);
   digitalWrite(ledPin, HIGH);
   delay(1000);
 
-  rtc.begin();
-  display.begin();
+  // display.begin();
 
   // //Set alarm1 every day at 18:33
   rtc.set_alarm(alarmNumber, 21, 06, 0);
@@ -73,47 +47,115 @@ void setup() {
 
 //------------------------------------------------------------
 
-void loop() {
-  static uint8_t buttonStateLeft = defaultButtonState;
-  static uint8_t buttonStateMid = defaultButtonState;
-  static uint8_t buttonStateRight = defaultButtonState;
+class UpdateDisplay : public TimedTask
+{
+public:
+  UpdateDisplay();
+  virtual void run(uint32_t now);
 
-  static uint32_t currentMillis;
-  static uint32_t previousMillisDisplay;
-  static uint32_t previousMillisSleep;
+private:
+  char bufferTimestamp[10];
+};
 
-  static uint8_t alarmFlag = 0;
-  static uint8_t ledStatus = 1;
+UpdateDisplay::UpdateDisplay() : TimedTask(millis())
+{
+  display.begin();
+  display.println("Hello World");
+}
 
-  static char bufferTimestamp[10];
+void UpdateDisplay::run(uint32_t now)
+{
+  display.clear();
+  rtc.get_timestamp_str(bufferTimestamp);
+  display.println(bufferTimestamp);
+  display.print("hello");
+  display.show();
 
-  currentMillis = millis();
+  incRunTime(1000);
+}
 
-  //On first loop we enter the sleep mode
-  if (alarmFlag == 0) {
-    // Wake from either RTC alarm or button press
+//------------------------------------------------------------
 
-    digitalWrite(ledPin, LOW);                             //switch-off the led for indicating that we enter the sleep mode
-    ledStatus = 0;                                         //set the led status accordingly
 
-    powerDown();
+class SleepMode : public TriggeredTask
+{
+public:
+  SleepMode();
+  virtual void run(uint32_t now);
 
-    wakeUp();
-    //When exiting the sleep mode we clear the alarm
-    rtc.clear_alarm(alarmNumber);
-    alarmFlag++;
-  }
+private:
+  bool sleepState;
+  void powerDown();
+  void wakeUp();
+  static void wakeUpInterruptHandler();
 
-  if (currentMillis - previousMillisDisplay >= intervalDisplayRefresh) {
-    display.clear();
-    rtc.get_timestamp_str(bufferTimestamp);
-    display.println(bufferTimestamp);
-    display.print("hello");
-    display.show();
+};
 
-    previousMillisDisplay += intervalDisplayRefresh;
-  }
+SleepMode::SleepMode() : TriggeredTask()
+{
+  //Set pin D2 as INPUT for accepting the interrupt signal from DS3231
+  pinMode(wakePin, INPUT);
+  rtc.begin();
+}
 
+void SleepMode::run(uint32_t now)
+{
+  powerDown();
+
+  wakeUp();
+  sleepState = false;
+
+  resetRunnable();
+}
+
+void SleepMode::powerDown() {
+  display.sleep();
+
+  attachInterrupt(digitalPinToInterrupt(wakePin), wakeUpInterruptHandler, LOW);
+  attachInterrupt(digitalPinToInterrupt(buttonPinMiddle), wakeUpInterruptHandler, CHANGE);
+
+  LowPower.powerDown(SLEEP_FOREVER, ADC_OFF, BOD_OFF);
+}
+
+void SleepMode::wakeUp() {
+  display.wake();
+
+  detachInterrupt(digitalPinToInterrupt(wakePin));                                    //execution resumes from here after wake-up
+  detachInterrupt(digitalPinToInterrupt(buttonPinMiddle));
+}
+
+//-------------------------------------------------
+
+void SleepMode::wakeUpInterruptHandler()        // here the interrupt is handled after wakeup
+{
+}
+
+
+//------------------------------------------------------------
+
+class UserInput : public TimedTask
+{
+public:
+  UserInput(SleepMode *_ptrSleep);
+  virtual void run(uint32_t now);
+
+private:
+  uint8_t buttonStateLeft = HIGH;
+  uint8_t buttonStateMid = HIGH;
+  uint8_t buttonStateRight = HIGH;
+
+  SleepMode *ptrSleep;
+};
+
+UserInput::UserInput(SleepMode *_ptrSleep) : TimedTask(millis()), ptrSleep(_ptrSleep)
+{
+    pinMode(buttonPinLeft, INPUT);
+    pinMode(buttonPinMiddle, INPUT);
+    pinMode(buttonPinRight, INPUT);
+}
+
+void UserInput::run(uint32_t now)
+{
   buttonStateLeft = digitalRead(buttonPinLeft);
   buttonStateMid = digitalRead(buttonPinMiddle);
   buttonStateRight = digitalRead(buttonPinRight);
@@ -121,6 +163,8 @@ void loop() {
   if (buttonStateMid == LOW) {
     // turn LED on:
     digitalWrite(ledPin, HIGH);
+
+    ptrSleep->setRunnable();
   } else {
     // turn LED off:
     digitalWrite(ledPin, LOW);
@@ -136,12 +180,26 @@ void loop() {
     motor.brake();
   }
 
-  if (currentMillis - previousMillisSleep >= intervalInactive) {
-    previousMillisSleep += intervalInactive;
+  incRunTime(100);
+}
 
-    powerDown();
+//------------------------------------------------------------
 
-    wakeUp();
-  }
+void loop() {
+
+  UpdateDisplay updateDisplay;
+  SleepMode sleepMode;
+  UserInput userInput(&sleepMode);
+
+  // Order determines task priority
+  Task *tasks[] = {
+    &userInput,
+    &updateDisplay,
+    &sleepMode
+  };
+
+  TaskScheduler scheduler(tasks, NUM_TASKS(tasks));
+
+  scheduler.runTasks();
 
 }
