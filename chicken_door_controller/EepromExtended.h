20c8605cab00d613919f9b9b75cac6299b7e785b
Justin Lam <justinmklam@gmail.com>
2019-04-09 12:15:45 -0700

    Move custom eeprom methods to separate file

---
+++ chicken_door_controller/EepromExtended.h
@@ -0,0 +1,39 @@
+#include <EEPROM.h>
+
+void EEPROMWrite8bit(uint8_t address, uint8_t value)
+{
+  EEPROM.write(address, value);
+}
+uint8_t EEPROMRead8bit(uint8_t address)
+{
+  return EEPROM.read(address);
+}
+void EEPROMWrite32bit(uint8_t address, uint32_t value)
+{
+  uint8_t bytes[4];
+
+  bytes[0] = (value >> 24) & 0xFF;
+  bytes[1] = (value >> 16) & 0xFF;
+  bytes[2] = (value >> 8) & 0xFF;
+  bytes[3] = value & 0xFF;
+
+  for (int i=0; i < 4; i++)
+  {
+    EEPROM.write(address + i, bytes[i]);
+  }
+}
+
+uint32_t EEPROMRead32bit(uint8_t address)
+{
+  uint32_t bytes[4];
+  uint32_t value;
+
+  for (int i=0; i < 4; i++)
+  {
+    bytes[i] = EEPROM.read(address + i);
+  }
+
+  value = (bytes[3] + (bytes[2] << 8) + (bytes[1] << 16) + (bytes[0] << 24));
+
+  return value;
+}
--- chicken_door_controller/chicken_door_controller.ino
+++ chicken_door_controller/chicken_door_controller.ino
@@ -1,6 +1,6 @@
 #include <LowPower.h>
-#include <EEPROM.h>
 
+#include "EepromExtended.h"
 #include "Task.h"
 #include "TaskScheduler.h"
 
@@ -24,46 +24,6 @@ const uint8_t buttonPinRight = 12;
 const uint8_t defaultButtonState = HIGH;
 
 const uint16_t intervalInactive = 100;
-
-/*************************************************************************/
-
-void EEPROMWrite8bit(uint8_t address, uint8_t value)
-{
-  EEPROM.write(address, value);
-}
-uint8_t EEPROMRead8bit(uint8_t address)
-{
-  return EEPROM.read(address);
-}
-void EEPROMWrite32bit(uint8_t address, uint32_t value)
-{
-  uint8_t bytes[4];
-
-  bytes[0] = (value >> 24) & 0xFF;
-  bytes[1] = (value >> 16) & 0xFF;
-  bytes[2] = (value >> 8) & 0xFF;
-  bytes[3] = value & 0xFF;
-
-  for (int i=0; i < 4; i++)
-  {
-    EEPROM.write(address + i, bytes[i]);
-  }
-}
-
-uint32_t EEPROMRead32bit(uint8_t address)
-{
-  uint32_t bytes[4];
-  uint32_t value;
-
-  for (int i=0; i < 4; i++)
-  {
-    bytes[i] = EEPROM.read(address + i);
-  }
-
-  value = (bytes[3] + (bytes[2] << 8) + (bytes[1] << 16) + (bytes[0] << 24));
-
-  return value;
-}
 
 /*************************************************************************/
 
